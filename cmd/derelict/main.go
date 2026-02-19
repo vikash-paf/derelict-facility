f5fb107dc29885757dcad42b0defd6ab6854d6c3
vikash <vkspaf@gmail.com>
2026-02-19 19:36:10 +0000

    create an empty map of 80x25

--- cmd/derelict/main.go
+++ cmd/derelict/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/vikash-paf/derelict-facility/internal/world"
 )
 
 func main() {
+	width, height := 80, 25
+	gameMap := world.NewMap(width, height)
+
+	for i := 0; i < width*height; i++ {
+		gameMap.Tiles[i] = world.Tile{Type: world.TileTypeFloor, Walkable: true}
+	}
+
 	fmt.Println("Starting derelict-facility engine...")
 }
