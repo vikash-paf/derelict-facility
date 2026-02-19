045b24b10a873afeb51d3d9fd1943ec27f799935
vikash <vkspaf@gmail.com>
2026-02-19 19:47:10 +0000

    spawn a player and check keyboard input

--- cmd/derelict/main.go
+++ cmd/derelict/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
+	"github.com/vikash-paf/derelict-facility/internal/entity"
 	"github.com/vikash-paf/derelict-facility/internal/terminal"
 	"github.com/vikash-paf/derelict-facility/internal/world"
 )
@@ -15,6 +18,9 @@ func main() {
 		gameMap.Tiles[i] = world.Tile{Type: world.TileTypeFloor, Walkable: true}
 	}
 
+	// create the player
+	player := entity.NewActor(10, 10, '@')
+
 	term := terminal.NewTerminal()
 	err := term.EnableRawMode()
 	if err != nil {
@@ -24,5 +30,30 @@ func main() {
 
 	term.Clear()
 
-	fmt.Println("Starting derelict-facility engine...")
+	// spawn the player
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			if player.X == x && player.Y == y {
+				fmt.Printf("%c", player.Char)
+			} else {
+				fmt.Printf(".")
+			}
+		}
+
+		fmt.Print("\r\n") // In raw mode, \n just moves down, \r moves to start of line
+	}
+
+	// read the input to verify if the raw mode works, 'q' to quit
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		r, _, err := reader.ReadRune()
+		if err != nil {
+			return
+		}
+
+		if r == 'q' {
+			break
+		}
+	}
+
 }
