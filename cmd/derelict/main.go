c83f9a6521df1f8125711043980b258bd9a6ad36
vikash <vkspaf@gmail.com>
2026-02-19 21:37:20 +0000

    populate walkable tiles

--- cmd/derelict/main.go
+++ cmd/derelict/main.go
@@ -10,11 +10,6 @@ import (
 
 func main() {
 	width, height := 80, 25
-	gameMap := world.NewMap(width, height)
-
-	for i := 0; i < width*height; i++ {
-		gameMap.Tiles[i] = world.Tile{Type: world.TileTypeFloor, Walkable: true}
-	}
 
 	term := terminal.NewTerminal()
 	err := term.EnableRawMode()
@@ -25,10 +20,19 @@ func main() {
 
 	gameEngine := engine.NewEngine(term, width, height)
 
+	for i := 0; i < width*height; i++ {
+		gameEngine.Map.Tiles[i] = world.Tile{Type: world.TileTypeFloor, Walkable: true}
+	}
+
 	// Draw a simple box of walls around the edges
 	for x := 0; x < 80; x++ {
 		gameEngine.Map.SetTile(x, 0, world.Tile{Type: world.TileTypeWall, Walkable: false})
-		gameEngine.Map.SetTile(x, 23, world.Tile{Type: world.TileTypeWall, Walkable: false})
+		gameEngine.Map.SetTile(x, 24, world.Tile{Type: world.TileTypeWall, Walkable: false})
+	}
+
+	for y := 0; y < 25; y++ {
+		gameEngine.Map.SetTile(0, y, world.Tile{Type: world.TileTypeWall, Walkable: false})
+		gameEngine.Map.SetTile(79, y, world.Tile{Type: world.TileTypeWall, Walkable: false})
 	}
 
 	err = gameEngine.Run()
