e0a18c50f5e07667a9bca1aff27d426ddc9afa61
vikash <vkspaf@gmail.com>
2026-02-19 19:12:43 +0000

    create an empty map

--- internal/world/map.go
+++ internal/world/map.go
@@ -8,3 +8,11 @@ type Map struct {
 	// this will store the location, it's continuous "tape", better performance and low memory usage
 	Tiles []Tile
 }
+
+func NewMap(width, height int) *Map {
+	return &Map{
+		Width:  width,
+		Height: height,
+		Tiles:  make([]Tile, width*height),
+	}
+}
