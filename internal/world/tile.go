eb587909eb6fb5a0dd3a88abb6703c1411a3bdaf
vikash <vkspaf@gmail.com>
2026-02-19 19:33:24 +0000

    move tiles into tile.go

--- internal/world/tile.go
+++ internal/world/tile.go
@@ -1,1 +1,14 @@
 package world
+
+type TileType uint8
+
+const (
+	TileTypeEmpty TileType = iota
+	TileTypeWall
+	TileTypeFloor
+)
+
+type Tile struct {
+	Type     TileType
+	Walkable bool
+}
--- internal/world/world.go
+++ internal/world/world.go
@@ -1,14 +1,1 @@
 package world
-
-type TileType uint8
-
-const (
-	TileTypeEmpty TileType = iota
-	TileTypeWall
-	TileTypeFloor
-)
-
-type Tile struct {
-	Type     TileType
-	Walkable bool
-}
