c84b03a11a38bc98dcf07c8f68425e294648216f
vikash <vkspaf@gmail.com>
2026-02-19 20:54:20 +0000

    implement Run main loop in engine

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -1,7 +1,9 @@
 package engine
 
 import (
+	"bytes"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/vikash-paf/derelict-facility/internal/entity"
@@ -30,7 +32,61 @@ func NewEngine(term *terminal.Terminal, width, height int) *Engine {
 
 // Run starts the deterministic game loop
 func (e *Engine) Run() error {
-	// TODO: implement game loop
+	inputChan := e.Terminal.PollInput()
+	ticker := time.NewTicker(e.TickerRate)
+	defer ticker.Stop()
+
+	width, height := e.Map.Width, e.Map.Height
+	memorySize := (width * height) + (height * 2) + 50 // bytes, a little bit extra memory
+	screen := bytes.NewBuffer(make([]byte, 0, memorySize))
+
+	for e.Running {
+		screen.Reset()
+
+		// move the cursor to the top left
+		screen.WriteString("\033[H")
+
+		// spawn the player
+		for y := 0; y < height; y++ {
+			for x := 0; x < width; x++ {
+				if e.Player.X == x && e.Player.Y == y {
+					screen.WriteRune(e.Player.Char)
+				} else {
+					screen.WriteString(".")
+				}
+			}
+
+			screen.WriteString("\r\n") // In raw mode, \n just moves down, \r moves to start of line
+		}
+
+		os.Stdout.Write(screen.Bytes())
+
+		// handle input and move player
+		select {
+		case event := <-inputChan:
+			e.handleInput(event)
+		default:
+			// todo: add fps limit (sleep here)
+		}
+
+		time.Sleep(16 * time.Millisecond) // ~60FPS
+	}
+
 	fmt.Println("Engine running...")
 	return nil
+}
+
+func (e *Engine) handleInput(event terminal.InputEvent) {
+	switch event.Key {
+	case 'w':
+		e.Player.Y--
+	case 's':
+		e.Player.Y++
+	case 'a':
+		e.Player.X--
+	case 'd':
+		e.Player.X++
+	case 'q':
+		e.Running = false
+	}
 }
