672579d3882ed220a82a08f1cc1d52d42e317c84
vikash <vkspaf@gmail.com>
2026-02-19 19:08:16 +0000

    add docs

--- internal/world/world.go
+++ internal/world/world.go
@@ -17,5 +17,8 @@ type Tile struct {
 type Map struct {
 	Width  int
 	Height int
-	Tiles  []Tile // keeping this as a single slice for now, this will store the location, it's continuous "tape"
+	
+	// keeping this as a single slice for now,
+	// this will store the location, it's continuous "tape", better performance and low memory usage
+	Tiles []Tile
 }
