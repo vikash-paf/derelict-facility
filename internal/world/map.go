78dad6b00fe27a9bc35ef1e414443e73413c26a6
vikash <vkspaf@gmail.com>
2026-02-19 19:15:10 +0000

    get a tile from the map

--- internal/world/map.go
+++ internal/world/map.go
@@ -9,6 +9,15 @@ type Map struct {
 	Tiles []Tile
 }
 
+func (m *Map) GetTile(x, y int) *Tile {
+	if x < 0 || x >= m.Width || y < 0 || y >= m.Height {
+		return nil
+	}
+
+	// basic math to calculate the index of a tile in the "tape"
+	return &m.Tiles[x+y*m.Width]
+}
+
 func NewMap(width, height int) *Map {
 	return &Map{
 		Width:  width,
