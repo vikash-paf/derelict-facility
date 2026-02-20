2626243e061d5aa50396cb53d49a2daacbc5ec8a
vikash <vkspaf@gmail.com>
2026-02-20 17:32:58 +0000

    change type and use pcg

--- internal/world/facility.go
+++ internal/world/facility.go
@@ -1,18 +1,18 @@
 package world
 
 import (
-	"math/rand"
+	"math/rand/v2"
 )
 
-func NewFacilityGenerator(seed int64) *FacilityGenerator {
+func NewFacilityGenerator(seed uint64) *FacilityGenerator {
 	return &FacilityGenerator{
 		seed: seed,
-		rng:  rand.New(rand.NewSource(seed)),
+		rng:  rand.New(rand.NewPCG(seed, seed)),
 	}
 }
 
 type FacilityGenerator struct {
-	seed int64
+	seed uint64
 	rng  *rand.Rand // random number generator
 }
 
