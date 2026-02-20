9a996b54399086dc9101cd1340ea452349fecedf
vikash <vkspaf@gmail.com>
2026-02-20 17:39:15 +0000

    add facility generation algo details

--- internal/world/facility.go
+++ internal/world/facility.go
@@ -18,8 +18,10 @@ type FacilityGenerator struct {
 
 func (f FacilityGenerator) Generate(width, height int) (*Map, int, int) {
 	// todo:
-	// 1. create the empty map
+	// 1. create the empty map, with walls (non walkable tiles)
 	// 2. run the generation algorithm (l-Corridors algorithm, aka Procedural Dungeon Generator)
+	// 2.1 carve the rooms
+	// 2.2 connect the rooms (l-corridors)
 	// 3. return the map and the player position (center of the first room)
 	return nil, 0, 0
 }
