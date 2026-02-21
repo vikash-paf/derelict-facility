f267a87841d3e9bb325e5ee4de29e5cdb9f0eb90
vikash <vkspaf@gmail.com>
2026-02-21 22:13:35 +0000

    add algos benchmarking suite

---
+++ internal/world/map_bench_test.go
@@ -0,0 +1,47 @@
+package world
+
+import (
+	"strings"
+	"testing"
+)
+
+func BenchmarkGetLine_Diagonal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = getLine(0, 0, 20, 20)
+	}
+}
+
+func BenchmarkComputeFOV_SmallRadius(b *testing.B) {
+	m := newTestMap(strings.Repeat(".", 80*40))
+	m.Width = 80
+	m.Height = 40
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.ComputeFOV(40, 20, 5)
+	}
+}
+
+func BenchmarkComputeFOV_LargeRadius(b *testing.B) {
+	m := newTestMap(strings.Repeat(".", 100*100))
+	m.Width = 100
+	m.Height = 100
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.ComputeFOV(50, 50, 20)
+	}
+}
+
+func BenchmarkMapReset_Large(b *testing.B) {
+	m := newTestMap(strings.Repeat(".", 100*100))
+	m.Width = 100
+	m.Height = 100
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for j := range m.Tiles {
+			m.Tiles[j].Visible = false
+		}
+	}
+}
