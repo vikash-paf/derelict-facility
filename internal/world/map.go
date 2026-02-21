6ab1a17b420f350c67adf6c5da91756f751c06ce
vikash <vkspaf@gmail.com>
2026-02-21 20:50:19 +0000

    add bounding box light (ray) casting pseudocode

--- internal/world/map.go
+++ internal/world/map.go
@@ -30,6 +30,28 @@ func (m *Map) GetTile(x, y int) *Tile {
 
 func (m *Map) ComputeFOV(playerX, playerY int, radius int) {
 	// 1. reset all tiles to not visible
+
+	for i := range m.Tiles {
+		m.Tiles[i].Visible = false
+	}
+
+	// 1.1 get the (light radius) bounding box
+	minX, maxX := playerX-radius, playerX+radius
+	minY, maxY := playerY-radius, playerY+radius
+
+	// 1.2 cast rays to the points in the bounding box (TOP and BOTTOM edges of the box)
+	for x := minX; x <= maxX; x++ {
+		// cast ray to top edge
+		// cast ray to bottom edge
+
+	}
+
+	// 1.2 cast rays to the points in the bounding box (TOP and BOTTOM edges of the box)
+	for y := minY; y <= maxY; y++ {
+		// cast ray to top edge
+		// cast ray to bottom edge
+	}
+
 	// 2. compute the visible tiles
 	// 3. mark all tiles in the radius as visible
 	// uses Bresenham's line algorithm
